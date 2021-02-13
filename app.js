a45b160ca6bd3dbdfcb7d07dde83439975e7bad2
kajj8808 <71279997+kajj8808@users.noreply.github.com>
2021-02-13 14:43:47 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const { DISCODE_TOKEN } = require("./config.json");
+const { DISCODE_TOKEN, PREFIX } = require("./config.json");
 const Discord = require("discord.js");
+const { execute } = require("./function/execute");
 
 const app = new Discord.Client();
 app.login(DISCODE_TOKEN);
 
 app.on("ready", () => {
-  console.log("botOnReady!");
+    console.log("botOnReady!");
 });
 
 app.on("message", async (msg) => {
-  if (msg.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
+    if (msg.author.bot) return;
+    if (!msg.content.startsWith(PREFIX)) return;
 
-  if (msg.content.startsWith("/play ")) {
-    if (msg.member.voice.channel) {
-      execute(msg);
+    const serverId = msg.guild.id;
+
+    if (msg.content.startsWith(`${PREFIX}play`)) {
+        if (msg.member.voice.channel) {
+            execute(msg, serverId, PREFIX);
+            return;
+        }
+    } else if (msg.content.startsWith(`${PREFIX}skip`)) {
+      //skip(serverId);
       return;
     } else {
-      msg.reply("You need to join a voice channel first!");
+      msg.reply("You need to enter a valid Command!");
     }
-  }
-
-  if (msg.content === "/skip") {
-    return;
-  }
-  
+    //msg.reply("You need to join a voice channel first!");
 });
-
-
